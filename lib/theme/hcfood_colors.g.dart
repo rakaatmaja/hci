9681952b454f8c3a98134a8210762be4bad22075
Parabeac-Nest <admin@parabeac.com>
2022-12-16 05:46:07 +0000

    Auto-generated commit by Parabeac Cloud

---
+++ lib/theme/hcfood_colors.g.dart
@@ -0,0 +1,18 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+// 
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+    
+
+import 'package:flutter/material.dart';
+class HcfoodColors {
+
+static const Color rodiniaMainColor = Color(0xff0b735f);
+
+static const Color textColor = Color(0xff1c1c1c);
+
+static const Color rodiniaError = Color(0xfff0406a);
+
+static const Color primaryGreen = Color(0xff0b735f);
+}
